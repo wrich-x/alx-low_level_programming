0e8f98fc3d9d384685267edd1f75bfe08b92ccbd
Rich Wafula <wrichx.online@gmail.com>
2023-02-16 12:50:19 -0800

    Task 1,2,3,4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+ * main - printing a line using puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	/* string intialisation */
+	char Mystring[] = "\"Programming is like building a multilingual puzzle";
+
+	/* writing string to stdout */
+	puts(Mystring);
+	return (0);
+}
