0badbb3b0f57f4550922bf7f7ae7bfc0c8b69f8e
Rich Wafula <wrichx.online@gmail.com>
2023-02-16 13:06:04 -0800

    Task 4-Hello Puts correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 /*
  * main - printing a line using puts
  *
+ * Description: a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ *
  * Return: 0
  */
 int main(void)
