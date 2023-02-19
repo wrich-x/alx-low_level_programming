6664ce1e9abc8d6312a7cecc0fdbebd3e28beb3e
Rich Wafula <wrichx.online@gmail.com>
2023-02-19 12:15:20 -0800

    sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * main - entry point
+ * wrx
+ * Description: assign a random number to the variable n
+ * Return: 0
+ */
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		x = tolower(x);
+		putchar("%c ", x)\n;
+	}
+	return (0);
+}
