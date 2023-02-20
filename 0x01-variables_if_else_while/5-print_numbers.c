26bcf4ed7973f3a34c1295511009cc4274282844
Rich Wafula <wrichx.online@gmail.com>
2023-02-20 12:22:17 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * wrx
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
