04155355086b622e4b97faee28e614e563257f0c
Rich Wafula <wrichx.online@gmail.com>
2023-02-23 12:07:30 -0800

    task 1 digit function commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for digits 0 - 9
+ * wrx
+ * @c: Character defined in int _isdigit(int c);
+ * Return: 1 (if true) else returns 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
