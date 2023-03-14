f2e8d1352f971259e0eaf1c4adb9384a7ee9e1da
Rich Wafula <wrichx.online@gmail.com>
2023-03-14 16:58:20 -0700

    this main and readme file and everything in between

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _islower - function to calulate lower value Entry point
+ * @c: integer to count
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,17 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
