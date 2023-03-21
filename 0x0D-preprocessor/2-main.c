23da052f9a95df2c961f9e56ec1ee09770b0ab99
Rich Wafula <wrichx.online@gmail.com>
2023-03-21 09:15:17 -0700

    task 2 edit 4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * File: 2-main.c
- * Auth: wr*
  * main - prints the name of the file of program
  * it was compiled from folowed by a new line.
- * Return: 0 shows succesful compilation
+ * Return: 0 Success
  */
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
