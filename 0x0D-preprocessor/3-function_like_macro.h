c1c03b75ee194c14d882fcc3ed0e54543f8746b6
Rich Wafula <wrichx.online@gmail.com>
2023-03-21 08:51:53 -0700

    task 3 edit 1

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -6,6 +6,7 @@
  * Auth: wr*
  * Desc: A header file that defines a function-like macro
  * ABS(x) which computes the absolute value of a number x.
+ * @x: character to compute
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
