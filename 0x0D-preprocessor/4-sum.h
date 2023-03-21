9b5aca9e4535e442f5c6c993075540b16bbd4b4b
Rich Wafula <wrichx.online@gmail.com>
2023-03-21 08:40:20 -0700

    Task 4. SUM, function like macro SUM(x, y)

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Auth: wr*
+ * Desc: A header file that defines a function-lie macro SUM(x, y)
+ * which computes the sum of the numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
