44836c3c929c179c1acbf0cce69ef6d4da418c7c
Rich Wafula <wrichx.online@gmail.com>
2023-03-21 08:15:01 -0700

    Task 2. File name prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * File: 2-main.c
+ * Auth: wr*
+ * main - prints the name of the file of program
+ * it was compiled from folowed by a new line.
+ * Return: 0 shows succesful compilation
+ */
+int main(void)
+{
+	printf("%\n", __FILE__);
+
+	return (0);
+}
