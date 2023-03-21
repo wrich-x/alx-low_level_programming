34de40a6abd9afe9bfdf40a9ccdf5cf4ca838cea
Rich Wafula <wrichx.online@gmail.com>
2023-03-21 07:56:26 -0700

    read me && Task-0. Object-like Macro commit

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Auth: wr*
+ * Desc: A Header file that defines
+ * a macro size as an abbreviation for token 1024
+ */
+
+#define SIZE 1024
+
+#endif
