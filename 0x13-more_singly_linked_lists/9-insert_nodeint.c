9fd6f551fbb25a41b572e2d320e8dbda1ee18718
wrich-x <wrichx.online@gmail.com>
2023-04-05 15:05:44 -0700

    function that inserts a new node at a given position edit1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position.
+ * insert_nodeint_at_index - function that
+ * inserts a new node at a given position.
  *
  * @head: head of the linked list
  * @idx: insext of the linkeed list
